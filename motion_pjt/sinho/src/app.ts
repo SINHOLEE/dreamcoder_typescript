3d25bcc041fbf04e98610a071cf0a3980cf36ee8
sinholee <dltlsgh5@gmail.com>
2021-06-08 07:08:09 +0900

    텀포넌트

--- motion_pjt/sinho/src/app.ts
+++ motion_pjt/sinho/src/app.ts
@@ -1,24 +1,53 @@
-console.log('hello word');
-class App<T> {
-	private data: T[];
-	private parent: Element;
-	private template: string | Element;
-	constructor(parent: Element, data: T[] = []) {
-		this.data = data;
-		this.parent = parent;
-		this.template = this.genarateTemplate();
+class Modal {}
+
+class Header {
+	private template: string;
+	private $parent: Element;
+	private $el: Element;
+	constructor(parent: Element, private data: string[]) {
+		this.$parent = parent;
+		this.template = this.genareateTemplate();
+		this.$el = this.genarateEl();
+		this.addClickListener();
 	}
-	genarateTemplate = () => {
-		return '';
+	genarateEl = () => {
+		const $el = document.createElement('div');
+		$el.className = 'main_header';
+		$el.innerHTML = this.template;
+		return $el;
 	};
+	addClickListener = () => {
+		const btnWrapper = this.$el.querySelector('.main__header__button__wrapper');
+		btnWrapper?.addEventListener('click', (e) => {
+			console.log(e.target);
+		});
+		console.log(btnWrapper);
+	};
+	onClick = () => {};
 	render = () => {
-		console.log(this.parent, 123);
-		this.parent.innerHTML = `
-    <div class='main'>
-    
-    </div>
-    `;
+		this.$parent.appendChild(this.$el);
+	};
+	genareateTemplate = () => {
+		return `
+    <div class="main__header__title">MOTION</div>
+    <div class="main__header__button__wrapper">
+${this.data.map((item) => `<button id=${item}>${item}</button>`).join('')}
+  </div>
+  `;
 	};
 }
-// const app = new App(document.querySelector('#app') as Element);
-// app.render();
+console.log('hello word');
+class App {
+	constructor(private $el: Element) {}
+	createHeader = () => {
+		const header = new Header(this.$el, ['image', 'video', 'note', 'task']);
+		header.render();
+	};
+	render = () => {
+		const main = document.createElement('div');
+		main.className = 'main';
+		this.createHeader();
+	};
+}
+const app = new App(document.querySelector('#app') as Element);
+app.render();
